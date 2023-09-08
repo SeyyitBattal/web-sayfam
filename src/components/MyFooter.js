13c3d1a1dfd34d3c427030a3652bf4bb5e2e9634
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-08 18:04:17 +0300

    Main skeleton was created

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -1,7 +1,28 @@
+import { footerData } from "../data/FooterData";
+
 const MyFooter = () => {
   return (
     <div>
-      <p>Burası footer</p>
+      <h1 className="mb-6 text-4xl font-bold">{footerData.footerSloganEN}</h1>
+      <label className="underline dark:text-pink-600">{footerData.mail}</label>
+      <a
+        href="#"
+        class="font-medium text-blue-600 dark:text-slate-950 hover:underline"
+      >
+        Personal Blog
+      </a>
+      <a
+        href="#"
+        class="font-medium text-blue-600 dark:text-green-500 hover:underline"
+      >
+        Github
+      </a>
+      <a
+        href="#"
+        class="font-medium text-blue-600 dark:text-blue-600 hover:underline"
+      >
+        Linkedin
+      </a>
     </div>
   );
 };
---
+++ src/data/FooterData.js
@@ -0,0 +1,5 @@
+export const footerData = {
+  footerSloganEN: "Let's work together on your next product.",
+  footerSloganTR: "Bir sonraki projenizde beraber olalım!",
+  mail: "seyyitbattalarvas3@gmail.com",
+};
