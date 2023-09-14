658d0a1863ca437de8f6ab837e01ab9e2a572d82
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-09-14 16:40:45 +0300

    Skills and Profile sections did responsive

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,14 +11,14 @@ export const Profile = () => {
       </h1>
 
       <div className="profileAndAbout">
-        <div>
+        <div className="profile">
           <h1 className="mb-2 lg:text-4xl text-indigo-700 dark:text-indigo-200 font-medium max-lg:text-3xl max-lg:text-center">
             {profileData[language].title}
           </h1>
 
           {myInfo.map((item, key) => (
             <div className="basic-info max-sm:pl-4 ">
-              <div className="profil-detay flex max-lg:w-[300px] lg:h-[222px] max-sm:mb-3 max-sm:pl-3 sm:mb-3 max-lg:mx-auto  ">
+              <div className="profil-detay flex  max-sm:mb-3 max-sm:pl-3 max-lg:mx-auto  ">
                 <div className=" lg:w-[150px] max-sm:text-lg pr-5 sm:text-lg  lg:text-xl h-full text-black dark:text-white font-medium  sm:m-0 ">
                   <p className=" pb-2">Doğum tarihi</p>
                   <p className=" pb-2">İkamet Şehri</p>
@@ -35,8 +35,7 @@ export const Profile = () => {
             </div>
           ))}
         </div>
-
-        <div className="about-box lg:w-1/2 sm:w-9/12 max-sm:w-9/12 max-lg:px-4  max-sm:m-auto  max-sm:px-4   max-sm:mt-5 sm:mx-auto  ">
+        <div className=" aboutMe about-box lg:w-1/2 sm:w-9/12 max-sm:w-9/12 max-lg:px-4  max-sm:m-auto  max-sm:px-4   max-sm:mt-5 sm:mx-auto  ">
           <h3 className="mb-2 lg:text-4xl text-indigo-700 dark:text-indigo-200 font-medium max-lg:text-3xl max-lg:text-center ">
             {profileData[language].hakkimda}
           </h3>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,19 +4,19 @@ const Skills = () => {
     <>
       <h1 className="mb-6 mt-20 text-5xl font-bold">{skillsData.title}</h1>
       <div className="skillsSection mb-6">
-        <div className="skills-js mr-20">
+        <div className="skills-js">
           <h1 className="mb-6 lg:text-4xl text-indigo-700 dark:text-indigo-200  font-medium max-lg:text-3xl max-lg:text-center">
             {skillsData.jsTitle}
           </h1>
           <p>{skillsData.paragraph}</p>
         </div>
-        <div className="skills-react mx-20">
+        <div className="skills-react mx-6">
           <h1 className="mb-6 lg:text-4xl text-indigo-700 dark:text-indigo-200 font-medium max-lg:text-3xl max-lg:text-center">
             {skillsData.reactTitle}
           </h1>
           <p>{skillsData.paragraph}</p>
         </div>
-        <div className="skills-node ml-20">
+        <div className="skills-node ">
           <h1 className="mb-6 lg:text-4xl text-indigo-700 dark:text-indigo-200 font-medium max-lg:text-3xl max-lg:text-center">
             {skillsData.nodeTitle}
           </h1>
